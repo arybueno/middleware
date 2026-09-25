d165d7b44b68ea33e7800050b0146b1f18a56034
ary.bueno <aryanebueno70@gmail.com>
2026-09-24 20:26:39 -0400

    Adiciona rota GET de tarefas

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const express = require('express')
 const app = express()
 const porta = 3000
 
+const tarefas = [
+    { id: 1, titulo: 'faze tarefa', concluida: false },
+    { id: 2, titulo: 'come', concluida: true },
+    { id: 3, titulo: 'dumi', concluida: false }
+]
+
 app.get('/', (req, res) => {
     res.send('API de Tarefas no ar')
+})
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas)
 })
 
 app.listen(porta, () => {
